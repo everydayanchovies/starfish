539847f38b972c9279e3bb763d9f33f4b8044ec0
Max Marshall <4356678+everydayanchovies@users.noreply.github.com>
2022-04-04 17:44:30 +0200

    Started work on importing data from backup.

--- search/management/commands/migrate_old_data.py
+++ search/management/commands/migrate_old_data.py
@@ -27,11 +27,12 @@ class Command(BaseCommand):
         parser.add_argument("path", type=str)
 
     def handle(self, *args, **options):
+        # Order matters! Person and glossary must be the first and second entries in this array.
         dump_filenames = [
             "export_person",  # item_ptr_id name
+            "search_glossary",  # item_ptr_id title text author_id
             "search_community",  # id name part_of_id
             "search_event",  # item_ptr_id title text date location author_id contact_id
-            "search_glossary",  # item_ptr_id title text author_id
             "search_goodpractice",  # item_ptr_id title text author_id
             "search_information",  # item_ptr_id title text author_id
             "search_item_tags",  # id item_id tag_id
@@ -53,7 +54,11 @@ class Command(BaseCommand):
                 map(getattr(self, fn), self.dumps[filename])
             )
 
-        print(self.dumps_as_objs)
+        for filename in dump_filenames:
+            fn = "import_" + filename.replace("search_", "").replace("export_", "")
+            if not hasattr(self, fn):
+                continue
+            getattr(self, fn)()
 
     def sql_to_strings(self, filepath):
         lines = []
@@ -78,6 +83,10 @@ class Command(BaseCommand):
             return None
 
         return data
+
+    """
+    BEGIN array to object
+    """
 
     def arr_to_project(self, arr):
         item_ptr_id, title, text, begin_date, end_date, author_id, contact_id = arr
@@ -157,14 +166,93 @@ class Command(BaseCommand):
             glossary=self.glossary_for_id(glossary_id),
         )
 
+    """
+    END array to object
+    """
+
     def person_for_id(self, pid):
         parrs = [x for x in self.dumps["export_person"] if x[0] == pid]
         if not parrs:
             return None
+
+        pliveobjs = [x for x in Person.objects.all() if x.name == parrs[0][1]]
+        if pliveobjs:
+            return pliveobjs[0]
+
+        pobjs = [
+            x for x in self.dumps_as_objs["export_person"] if x.name == parrs[0][1]
+        ]
+        if pobjs:
+            return pobjs[0]
+
+        print("\nWarning! No person found for pid " + pid)
+
         return self.arr_to_person(parrs[0])
 
     def glossary_for_id(self, gid):
         garrs = [x for x in self.dumps["search_glossary"] if x[0] == gid]
         if not garrs:
             return None
+
+        gliveobjs = [x for x in Glossary.objects.all() if x.title == garrs[0][1]]
+        if gliveobjs:
+            return gliveobjs[0]
+
+        gobjs = [
+            x for x in self.dumps_as_objs["search_glossary"] if x.title == garrs[0][1]
+        ]
+        if gobjs:
+            return gobjs[0]
+
+        print("\nWarning! No glossary found for gid " + gid)
+
         return self.arr_to_glossary(garrs[0])
+
+    """
+    BEGIN import object
+    """
+
+    def import_project(self):
+        pass
+
+    def import_person(self):
+        live_persons = Person.objects.all()
+        for pi in self.dumps_as_objs["export_person"]:
+            for pj in live_persons:
+                if pi.name == pj.name:
+                    break
+            else:
+                print("Adding person " + pi.name)
+                pi.save()
+
+    def import_community(self):
+        pass
+
+    def import_event(self):
+        pass
+
+    def import_glossary(self):
+        live_glossaries = Glossary.objects.all()
+        for gi in self.dumps_as_objs["search_glossary"]:
+            for gj in live_glossaries:
+                if gi.title == gj.title:
+                    break
+            else:
+                print("Adding glossary " + gi.title)
+                gi.save()
+
+    def import_goodpractice(self):
+        pass
+
+    def import_information(self):
+        pass
+
+    def import_question(self):
+        pass
+
+    def import_tag(self):
+        pass
+
+    """
+    END import object
+    """

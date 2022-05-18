57450dd34e57b9e188ac39ee2f2e0c8ce9488c5d
everydayanchovies <4356678+everydayanchovies@users.noreply.github.com>
2022-05-18 15:26:01 +0200

    cache scripts

---
+++ scripts/cache/warmup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -x
+
+curl https://starfish-education.eu > /dev/null
+curl https://starfish-education.eu/browse > /dev/null
+curl https://starfish-education.eu/browse?ftype=goodpractice > /dev/null
+curl https://starfish-education.eu/browse?ftype=project > /dev/null
+curl https://starfish-education.eu/browse?ftype=event > /dev/null
+curl https://starfish-education.eu/browse?ftype=glossary > /dev/null
+curl https://starfish-education.eu/browse?ftype=information > /dev/null
+curl https://starfish-education.eu/browse?ftype=person > /dev/null
+curl https://starfish-education.eu/browse?ftype=question > /dev/null
+curl https://starfish-education.eu/browse?ftype=usercase > /dev/null

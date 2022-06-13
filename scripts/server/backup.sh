3851e6520ba53fce360f921d4f545c4b3cefa201
everydayanchovies <4356678+everydayanchovies@users.noreply.github.com>
2022-06-13 09:32:26 +0200

    backup script

--- scripts/server/backup.sh
+++ scripts/server/backup.sh
@@ -10,3 +10,5 @@ source venvdj3/bin/activate
 set -x
 
 python manage.py backup_database /home/ubuntu/Starfish-master/db.sqlite
+
+\ls -1 . | grep ".*.sqlite" | grep -vw "db.sqlite" | xargs -I{} mv {} /home/ubuntu/backups/

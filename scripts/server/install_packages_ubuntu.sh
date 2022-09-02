d23ff3c7628929f099f0feb61165426583bebe1f
everydayanchovies <4356678+everydayanchovies@users.noreply.github.com>
2022-09-02 16:36:25 +0200

    added ubuntu package

--- scripts/server/install_packages_ubuntu.sh
+++ scripts/server/install_packages_ubuntu.sh
@@ -8,4 +8,5 @@ fi
 sudo apt install -y \
     build-essential \
     python3 python3-dev python3-venv python3-pip \
-    libsasl2-dev libldap2-dev libssl-dev
+    libsasl2-dev libldap2-dev libssl-dev \
+    nginx
